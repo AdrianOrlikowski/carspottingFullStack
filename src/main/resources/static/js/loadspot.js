c12f2536ee41755de6a2adc8f230ecc5448ad147
AdrianOrlikowski <adr.orlikow@gmail.com>
2022-04-17 22:09:58 +0200

    #Added finding spots by user

--- src/main/resources/static/js/loadspot.js
+++ src/main/resources/static/js/loadspot.js
@@ -1,6 +1,6 @@
 function loadSpot(apiSpot) {
     //Object destructuring
-    let {carMake, carModel, appUserUsername, dateTime, picURL} = apiSpot;
+    let {carMake, carModel, appUserUsername, dateTime, picURL, spotId} = apiSpot;
     //Creating post
     let spot = document.createElement("div");
     spot.classList.add("spots");
@@ -13,13 +13,17 @@ function loadSpot(apiSpot) {
     let postedBy = document.createElement("p");
     postedBy.innerHTML =`<small>Posted by: ${appUserUsername} on ${dateString}</small>`;
     spot.appendChild(postedBy);
-
+    
     let line = document.createElement("hr");
     spot.appendChild(line);
 
     let carPic = document.createElement("p");
     carPic.innerHTML = `<img src="${picURL}" alt="Failed to load picture">`;
     spot.appendChild(carPic);
+
+    let idMark = document.createElement("p");
+    idMark.innerHTML = `<small>SpotId: ${spotId}</small>`;
+    spot.appendChild(idMark);
 
     postArea.appendChild(spot);
 }
